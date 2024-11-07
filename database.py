12a454e99f63dae3ed9b3e5f50499a8fa3b2b4a4
Sam Rogers <sam.0110.rogers@gmail.com>
2024-11-06 23:49:49 -0600

    use unix epoch timestamp

--- database.py
+++ database.py
@@ -7,8 +7,8 @@ database = Database(DATABASE_URL)
 
 async def init_database(db: Database) -> None:
     await db.execute("""
-CREATE TABLE IF NOT EXISTS notes (
+CREATE TABLE IF NOT EXISTS notes(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created_at INTEGER DEFAULT CURRENT_TIMESTAMP,
-    content TEXT
+    created_at TEXT DEFAULT (strftime('%s', 'now')),
+    content TEXT NOT NULL
 );""")
