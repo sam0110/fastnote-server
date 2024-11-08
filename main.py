83fa6df027750356226597810d31c73b69989efe
Sam Rogers <sam.0110.rogers@gmail.com>
2024-11-08 11:37:15 -0600

    rename endpoint methods

--- main.py
+++ main.py
@@ -16,14 +16,20 @@ async def lifespan(_: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
+@app.post("/note", response_model=notes.Note)
+async def post_note(client_note: notes.ClientNote):
+    note = await notes.create_note(database, client_note)
+    return note
+
+
 @app.get("/notes", response_model=List[notes.Note])
-async def read_notes():
+async def get_notes():
     note_list = await notes.get_notes(database)
     return note_list
 
 
 @app.get("/note/{note_id}", response_model=notes.Note)
-async def read_note(note_id: int):
+async def get_note(note_id: int):
     note = await notes.get_note(database, note_id)
     return note
 
@@ -31,10 +37,4 @@ async def read_note(note_id: int):
 @app.delete("/note/{note_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_note(note_id: int):
     await notes.delete_note(database, note_id)
-
-
-@app.post("/note", response_model=notes.Note)
-async def post_note(client_note: notes.ClientNote):
-    note = await notes.create_note(database, client_note)
-    return note
 
